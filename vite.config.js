93c245c9dbd330e5e19e7ee66f3a76a11029c678
Haroun-educ <your-email@example.com>
2025-04-27 01:13:49 +0100

    Fix base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // Using the correct GitHub repository name for deployment
-  base: '/portfolio/',
+  base: '/portfolio3d/',
 })
