c4c49108ad0e2f35d47acdbdacc5bf960242ebf7
Haroun-educ <your-email@example.com>
2025-04-27 01:36:22 +0100

    Simplify deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Using the correct GitHub repository name for deployment
-  base: '/portfolio3d/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    emptyOutDir: true,
-    sourcemap: false,
-  },
+  base: './'
 })
